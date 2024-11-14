4994ab12f7c8e602d5532cd41193228ce6fc41ff
tavvarez <gabrieldaraujotavares@gmail.com>
2024-11-14 15:08:54 -0300

    finalizado geração de DOCCOB para xml com apenas 1 nota. Próx passo: incrementar notas

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import datetime
 from datetime import datetime
 from datetime import date
 from src.services import filial 
+from src.services import vencimentos 
 import xml.etree.ElementTree as ET
 import re
 
@@ -17,6 +18,7 @@ def parse_cte_xml(file_path):
             "numero_cte": ide.find('./ns:nCT', namespaces=ns).text,
             "bialog": root.find('.//ns:emit/ns:xNome', namespaces=ns).text,
             "tomador": root.find('.//ns:rem/ns:xNome', namespaces=ns).text,
+            "cnpjTomador": root.find('.//ns:rem/ns:CNPJ', namespaces=ns).text,
             "cnpjBialog": root.find('.//ns:emit/ns:CNPJ', namespaces=ns).text,
             "icms": root.find('.//ns:Comp/ns:vComp', namespaces=ns).text,
             "valReceber": root.find('.//ns:vPrest/ns:vRec', namespaces=ns).text,
@@ -28,55 +30,98 @@ def parse_cte_xml(file_path):
             "numCte": root.find('.//ns:ide/ns:nCT', namespaces=ns).text,
             "serieCte": root.find('.//ns:ide/ns:serie', namespaces=ns).text,
             "remetente": root.find('.//ns:rem/ns:CNPJ', namespaces=ns).text,
+            "destinatario": root.find('.//ns:dest/ns:CNPJ', namespaces=ns).text,
         }
         data.append(item)
+
+        quantidade_cte = str(item["numCte"])
+        item["qtdRegistros"] = quantidade_cte.count(item["numCte"])
+
         cnpj = re.sub(r'\D', '', item["cnpjBialog"])
         filial_resultado = filial.identificar_filial(cnpj)
-        item["codigo_filial"] = filial_resultado 
-
+        item["codigo_filial"] = filial_resultado
+        cnpjTomador = re.sub(r'\D', '', item["cnpjTomador"])
+        data_emissao = datetime.strptime(item["dataEmissao"], "%Y-%m-%dT%H:%M:%S%z")
+        vencimento_resultado = vencimentos.calcular_vencimento(cnpjTomador, data_emissao)
+        item["vencimento"] = vencimento_resultado
     return data
+
+#Usar ljust() para preencher com brancos
+
+# Variáveis úteis
+tipoNota = "0"
+tipoDoc = "CTE"
+tipoCobranca = "BCO"
+idDeRegistro0 = "000"
+idDeRegistro00 = "350"
+idDeRegistro1 = "351"
+idDeRegistro2 = "352"
+idDeRegistro3 = "353"
+idDeRegistro4 = "354"
+idDeRegistro5 = "355"
+idDeInterCambio = "COB"
+idDeDocumento = "COBRA"
+banco = "BANCO COOPERATIVO SICREDI ARACAJU  2102 11535     5 I"
+
 
 def format_conemb_line(data):
     arrayDeContent = []
     for item in data:
         data_emissao = datetime.strptime(item['dataEmissao'], "%Y-%m-%dT%H:%M:%S%z")
         data_formatada = data_emissao.strftime("%d%m%y%H%M")
+        somente_data = data_emissao.strftime("%d%m%y")
         linhaTeste = (
-                      f"{item['bialog'].zfill(37)}"
+                      f"{item['bialog'].zfill(37)}" + " " +
                       f"{item['tomador'].zfill(11)}" + "                     " +
                       f"{data_formatada}"
-                      + "COB" +
-                      f"{data_formatada}" + "9" + "\n"
-                      f"{data_formatada}"
-                      + f"COBRA" +
+                      + idDeInterCambio +
+                      f"{data_formatada}" + "9" + "\n" 
+                      + idDeRegistro00 + idDeDocumento +
                       f"{data_formatada}" + "\n" + "0"
-                      "351" + f"{item['cnpjBialog'].zfill(14)}"
-                      f"{item['bialog'].zfill(34)}" + "\n" +
-                      "352" + f"{item['codigo_filial']}" + "\n"
-                      f"{item['icms'].zfill(10)}" + "\n"
-                      f"{item['valReceber'].zfill(10)}" + "\n"
-                      f"{item['valPrestServ'].zfill(10)}" + "\n"
-                      f"{item['valMercadoria'].zfill(10)}" + "\n"
-                      f"{item['peso'].zfill(10)}" + "\n"
-                      f"{item['chaveNf'].zfill(10)}" + "\n"
-                      f"{data_formatada}" + "\n"
-                      f"{item['numCte'].zfill(10)}" + "\n"
-                      f"{item['serieCte'].zfill(10)}" + "\n"
-                      f"{item['remetente'].zfill(10)}" + "\n"
+                      + idDeRegistro1 + f"{item['cnpjBialog'].zfill(14)}"
+                      f"{item['bialog'].zfill(34).ljust(113)}" + "\n"
+                      + idDeRegistro2 +
+                      f"{item['codigo_filial'].ljust(14)}"
+                      + tipoDoc + f"{item['numCte'].zfill(10)}" +
+                      f"{somente_data}" +
+                      f"{item["vencimento"]}"
+                      f"{item['valReceber'].zfill(13)}"
+                      f"{tipoCobranca.zfill(5)}"
+                      f"{item['icms']}" + "\n"
+                      f"{banco.zfill(91)}" + "\n"
+                      + idDeRegistro3 +
+                      f"{item["codigo_filial"].ljust(10)}"
+                      f"{item['serieCte'].ljust(6)}" + 
+                      f"{somente_data.zfill(23)}" + 
+                      f"{item["cnpjTomador"]}"
+                      f"{item["destinatario"]}"
+                      f"{item["cnpjBialog"]}" + "\n"
+                      + idDeRegistro4.ljust(5) + f"{item["chaveNf"][26:34]}"
+                      f"{somente_data}"
+                      f"{item['peso']}"
+                      f"{item['valMercadoria']}"
+                      f"{item["cnpjTomador"]}" + "\n"
+                      + idDeRegistro5 + "0" +
+                      f"{item["qtdRegistros".zfill(8)]}"
+                      f"{item['valReceber'].zfill(13)}"
                       )
     arrayDeContent.append(linhaTeste)
 
     return arrayDeContent
-    
+
 dataAtual = date.today()
-def generate_conemb(data, output_path=f"DOCCOB{dataAtual}.txt"):
+def generate_conemb(data, output_path=None):
+    for item in data:
+        nomeTomador = item["tomador"]
+    if output_path is None:
+        output_path = f"DOCCOB{nomeTomador}_{dataAtual}.txt"
     with open(output_path, "w") as file:
         linhas_formatadas = format_conemb_line(data)
         for linha in linhas_formatadas:
             file.write(linha + "\n")
 
 def main():
-    xml_file = "./XML/CTE 24379.xml"
+    xml_file = "./XML/CTE 45143.xml"
     data = parse_cte_xml(xml_file)
     generate_conemb(data)
     print(f"Gerado o EDI em {dataAtual}")
